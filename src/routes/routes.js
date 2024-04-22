dbe8c25d198468c49a173d22d2ecc3951ac784c1
Sthery <sthery.styll@gmail.com>
2024-04-22 14:51:14 +0100

    Rota e Schema Produto

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,9 @@ function routes(app) {
     app.use('/', require('./index'));
 
     app.use('/cliente', require('./cliente/rotasCliente'))
+    app.use('/produto', require('./produto/rotasProduto'))
+    /*app.use('/estoque', require('./estoque/rotasEstoque'))
+    app.use('/venda', require('./venda/rotasVenda'))*/
   }
   
   module.exports = routes;
